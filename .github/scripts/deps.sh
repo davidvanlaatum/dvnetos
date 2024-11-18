7cf4f8d78536471360e66d54b37491a18f3d1e57
David Van Laatum <david@vanlaatum.id.au>
2024-11-18 21:35:23 +1030

    initial

--- .github/scripts/deps.sh
+++ .github/scripts/deps.sh
@@ -4,9 +4,20 @@ set -e
 
 env
 
+cat > /etc/apt/sources.list.d/kitware.sources <<EOF
+Types: deb
+URIs: https://apt.kitware.com/ubuntu/
+Suites: noble
+Components: main
+Signed-By: /usr/share/keyrings/kitware-archive-keyring.gpg
+EOF
+
+test -f /usr/share/doc/kitware-archive-keyring/copyright ||
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
+
 apt update
 apt remove clang-16 clang-17
-PACKAGES="libc++-dev ninja-build python3-venv gcc-arm-none-eabi gcc-x86-64-linux-gnu valgrind"
+PACKAGES="cmake libc++-dev ninja-build python3-venv gcc-arm-none-eabi gcc-x86-64-linux-gnu valgrind"
 if [[ $MATRIX_CLANG == "ON" ]]; then
     PACKAGES="${PACKAGES} clang lld libclang-dev"
 fi
