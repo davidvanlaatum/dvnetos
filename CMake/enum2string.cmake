00340b79bb35a6305c2ae1cab4a362ec2a023173
David Van Laatum <david@vanlaatum.id.au>
2024-12-08 16:44:50 +1030

    initial

--- CMake/enum2string.cmake
+++ CMake/enum2string.cmake
@@ -7,15 +7,17 @@ function(enum2string)
     message(FATAL_ERROR "enum2string: TARGET not specified")
   endif ()
   get_filename_component(HEADER_NAME ${ARG_HEADER} NAME_WE)
-  add_custom_command(
-      OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/${HEADER_NAME}_tostring.cpp ${CMAKE_CURRENT_SOURCE_DIR}/${HEADER_NAME}_tostring.h
-      COMMAND ${PYTHON_VENV} ${ENUM2STRING} ${ARG_HEADER} ${HEADER_NAME}_tostring.cpp ${HEADER_NAME}_tostring.h -std=c++$<TARGET_PROPERTY:${ARG_TARGET},CXX_STANDARD> "-I$<JOIN:$<TARGET_PROPERTY:${ARG_TARGET},INCLUDE_DIRECTORIES>,;-I>"
-      COMMAND_EXPAND_LISTS
-      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-      DEPENDS ${ARG_HEADER} ${ENUM2STRING}
-  )
-  add_custom_target(generate-${HEADER_NAME}-tostring DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${HEADER_NAME}_tostring.cpp ${CMAKE_CURRENT_SOURCE_DIR}/${HEADER_NAME}_tostring.h)
-  add_dependencies(${ARG_TARGET} generate-${HEADER_NAME}-tostring)
-  target_sources(${ARG_TARGET} PRIVATE ${HEADER_NAME}_tostring.cpp ${HEADER_NAME}_tostring.h)
-  add_dependencies(generate-${HEADER_NAME}-tostring venv)
+  if (TARGET ${ARG_TARGET})
+    add_custom_command(
+        OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/${HEADER_NAME}_tostring.cpp ${CMAKE_CURRENT_SOURCE_DIR}/${HEADER_NAME}_tostring.h
+        COMMAND ${PYTHON_VENV} ${ENUM2STRING} ${ARG_HEADER} ${HEADER_NAME}_tostring.cpp ${HEADER_NAME}_tostring.h -std=c++$<TARGET_PROPERTY:${ARG_TARGET},CXX_STANDARD> "-I$<JOIN:$<TARGET_PROPERTY:${ARG_TARGET},INCLUDE_DIRECTORIES>,;-I>"
+        COMMAND_EXPAND_LISTS
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        DEPENDS ${ARG_HEADER} ${ENUM2STRING}
+    )
+    add_custom_target(generate-${HEADER_NAME}-tostring DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${HEADER_NAME}_tostring.cpp ${CMAKE_CURRENT_SOURCE_DIR}/${HEADER_NAME}_tostring.h)
+    add_dependencies(${ARG_TARGET} generate-${HEADER_NAME}-tostring)
+    add_dependencies(generate-${HEADER_NAME}-tostring venv)
+  endif ()
+  cus_target_sources(${ARG_TARGET} PRIVATE ${HEADER_NAME}_tostring.cpp ${HEADER_NAME}_tostring.h)
 endfunction()
