cee4ec07c3e3c175d915a658f24faa3e48408f39
David Van Laatum <david@vanlaatum.id.au>
2024-12-23 12:18:55 +1030

    initial

--- CMake/arch-alias.cmake
+++ CMake/arch-alias.cmake
@@ -1,5 +1,5 @@
 function(handleArchAlias var)
-    if (${${var}} STREQUAL "arm64")
+  if (${var} AND "${${var}}" STREQUAL "arm64")
         set(${var} "aarch64" PARENT_SCOPE)
     endif ()
 endfunction()
